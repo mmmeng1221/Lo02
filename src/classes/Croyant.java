cd0e3066ad7c5d27ba10090f5987606770cfc0e0
zyt9749 <zhangyuetong9749@outlook.com>
2016-12-04 16:25:08 +0100

    yuetong message2

--- src/classes/Croyant.java
+++ src/classes/Croyant.java
@@ -8,7 +8,7 @@ import java.util.Set;
  */
 public class Croyant extends Carte{
 
-    public Croyant(String nom, String capacite, int nbcroyant, Set<Integer> dogmes, Constants origine, Sacrifier method){
+    public Croyant(String nom, String capacite, int nbcroyant, Set<Integer> dogmes, int origine, Sacrifier method){
 
 
 
--- src/classes/Factory.java
+++ src/classes/Factory.java
@@ -23,16 +23,112 @@ public class Factory {
             }
         });
     public Croyant createMoins1(){
-        //Constants Moins = new Constants;
         Set<Integer> dogmes = new HashSet<Integer>();
         dogmes.add(Constants.DOGMES_HUMAIN);
         dogmes.add(Constants.DOGMES_NATURE);
         dogmes.add(Constants.DOGMES_MYSTIQUES);
         return new Croyant("Moins", "Donner un point d'action d'origine Jour.",2,dogmes,Constants.ORIGINE_JOUR,new Sacrifier(){
         @Override
-                public void sacrifier(Parmas parmas) {
+                public void sacrifier(Parameters parameters) {
 
         }
+        }
+    }
+    public Croyant createMoins2(){
+        Set<Integer> dogmes = new HashSet<Integer>();
+        dogmes.add(Constants.DOGMES_MYSTIQUES);
+        dogmes.add(Constants.DOGMES_HUMAIN);
+        dogmes.add(Constants.DOGMES_CHAOS);
+        return new Croyant("Moins", "Donner un point d'action d'origine Jour.",2,dogmes,Constants.ORIGINE_JOUR,new Sacrifier(){
+            @Override
+            public void sacrifier(Parameters parameters) {
+
+            }
+        }
+    }
+    public Croyant createMoins3(){
+        Set<Integer> dogmes = new HashSet<Integer>();
+        dogmes.add(Constants.DOGMES_SYMBOLES);
+        dogmes.add(Constants.DOGMES_MYSTIQUES);
+        dogmes.add(Constants.DOGMES_CHAOS);
+        return new Croyant("Moins", "Donner un point d'action d'origine Jour.",2,dogmes,Constants.ORIGINE_JOUR,new Sacrifier(){
+            @Override
+            public void sacrifier(Parameters parameters) {
+
+            }
+        }
+    }
+    public Croyant createMoins4(){
+        Set<Integer> dogmes = new HashSet<Integer>();
+        dogmes.add(Constants.DOGMES_MYSTIQUES);
+        dogmes.add(Constants.DOGMES_NATURE);
+        dogmes.add(Constants.DOGMES_SYMBOLES);
+        return new Croyant("Moins", "Donner un point d'action d'origine Jour.",2,dogmes,Constants.ORIGINE_JOUR,new Sacrifier(){
+            @Override
+            public void sacrifier(Parameters parameters) {
+
+            }
+        }
+    }
+    public Croyant createMoins5(){
+        Set<Integer> dogmes = new HashSet<Integer>();
+        dogmes.add(Constants.DOGMES_MYSTIQUES);
+        dogmes.add(Constants.DOGMES_NATURE);
+        dogmes.add(Constants.DOGMES_CHAOS);
+        return new Croyant("Moins", "Donner un point d'action d'origine Jour.",2,dogmes,Constants.ORIGINE_JOUR,new Sacrifier(){
+            @Override
+            public void sacrifier(Parameters parameters) {
+
+            }
+        }
+    }
+    public Croyant createTravailleurs1(){
+        //Constants Moins = new Constants;
+        Set<Integer> dogmes = new HashSet<Integer>();
+        dogmes.add(Constants.DOGMES_SYMBOLES);
+        dogmes.add(Constants.DOGMES_HUMAIN);
+        dogmes.add(Constants.DOGMES_CHAOS);
+        return new Croyant("Travailleurs", "Empêche une Divinité possédant le Dogme Nature ou Mystique de sacrifier une de ses cartes de Croyants durant ce tour.",2,dogmes,Constants.ORIGINE_JOUR,new Sacrifier(){
+            @Override
+            public void sacrifier(Parameters parameters) {
+
+            }
+        }
+    }
+    public Croyant createTravailleurs2(){
+        Set<Integer> dogmes = new HashSet<Integer>();
+        dogmes.add(Constants.DOGMES_HUMAIN);
+        dogmes.add(Constants.DOGMES_NATURE);
+        dogmes.add(Constants.DOGMES_SYMBOLES);
+        return new Croyant("Travailleurs", "Empêche une Divinité possédant le Dogme Chaos ou Mystique de sacrifier un de ses Guides Spirituels durant ce tour.",2,dogmes,Constants.ORIGINE_JOUR,new Sacrifier(){
+            @Override
+            public void sacrifier(Parameters parameters) {
+
+            }
+        }
+    }
+    public Croyant createTravailleurs3(){
+        Set<Integer> dogmes = new HashSet<Integer>();
+        dogmes.add(Constants.DOGMES_MYSTIQUES);
+        dogmes.add(Constants.DOGMES_HUMAIN);
+        dogmes.add(Constants.DOGMES_CHAOS);
+        return new Croyant("Travailleurs", "Vous piochez deux cartes au hasard dans la main d'une autre Divinité.",2,dogmes,Constants.ORIGINE_JOUR,new Sacrifier(){
+            @Override
+            public void sacrifier(Parameters parameters) {
+
+            }
+        }
+    }
+    public Croyant Ermite(){
+        Set<Integer> dogmes = new HashSet<Integer>();
+        dogmes.add(Constants.DOGMES_MYSTIQUES);
+        dogmes.add(Constants.DOGMES_NATURE);
+        dogmes.add(Constants.DOGMES_CHAOS);
+        return new Croyant("Ermite", "mpose le sacrifice d'un Croyant d'un autre joueur, qui choisit la carte. La capacité spéciale du sacrifice est jouée.",1,dogmes,Constants.ORIGINE_JOUR,new Sacrifier(){
+            @Override
+            public void sacrifier(Parameters parameters) {
+
+            }
         }
     }
     public Apocalypse creatApo(){
