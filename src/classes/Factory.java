bfe7a5bbb55dcc71a8d76f441e874bf1ff6305c4
zyt9749 <zhangyuetong9749@outlook.com>
2016-12-05 18:42:03 +0100

    写完了ermites，croyant的class已修正

--- src/classes/Factory.java
+++ src/classes/Factory.java
@@ -147,7 +147,7 @@ public class Factory {
         return new Croyant(nom,capacite,nbcroyant,dogmes,origine,new Sacrifier() {
             @Override
             public void sacrifier(Parameters parameters) {
-                parameters.getMyself().getCarteGuide().
+                parameters.getMyself().getCarteGuide()
 
             }
         }
